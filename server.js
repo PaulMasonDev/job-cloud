ba46a02f5bef6596c2c5e7dfbf1ef15e7468dc5d
Paul <webdevpaulmason@gmail.com>
2020-09-29 10:34:06 -0700

    Init get route to Indreed

--- server.js
+++ server.js
@@ -22,6 +22,17 @@ if (environment !== 'dev') {
   });
 }
 
+app.get('/', (req, res) => {
+  axios.get(`https://indreed.herokuapp.com/api/jobs?q=Software%20Developer`)
+    .then(response => {
+      for (let i = 0; i < response.data.length; i++) {
+        console.log(response.data[i].url);
+      }
+    })
+    .catch(err => console.log(err));
+  res.send('HI');
+});
+
 app.listen(process.env.PORT || port, (req, res) => {
   console.log(`Connected on port ${port}`);
 });
